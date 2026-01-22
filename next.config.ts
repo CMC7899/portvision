42c7b5179aa86b19fd2eab97c92ce5d8ce9ae5e2
Thanh Nguyen <thanh1221221@gmail.com>
2026-01-22 10:35:17 +0000

    when deploy app to my domain 4ee819a6.portvision-app.pages.dev. I clicke

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        // Applies to all routes in the app
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://4ee819a6.portvision-app.pages.dev" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      },
+    ];
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
